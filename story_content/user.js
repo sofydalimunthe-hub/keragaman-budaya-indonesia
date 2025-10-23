4442ec9ef1b972e8679e38447589e4dc3a7a30dd
adeliasaragih27-a11y <adeliasaragih27@gmail.com>
2025-10-23 23:12:17 +0700

    keragaman budaya indonesia

---
+++ story_content/user.js
@@ -0,0 +1,25 @@
+window.InitUserScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+window.Script1 = function()
+{
+  var audio = document.getElementById('bgSongku');
+audio.src="musik.mp3";
+audio.load();
+audio.play();
+audio.volume=0.4;
+}
+
+};
